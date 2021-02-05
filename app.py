d12ac5f129522790f0621d3a65e8c3637cef7759
Fadly Tanjung <tanjungfadly96@gmail.com>
2021-02-05 18:09:35 +0700

    Update Cache Image

--- app.py
+++ app.py
@@ -15,6 +15,16 @@ ALLOWED_EXTENSIONS = set(['png','jpg'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+# No caching at all for API endpoints.
+@app.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
 
 def allowed_file(filename):
     return '.' in filename and \
